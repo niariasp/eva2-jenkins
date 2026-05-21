5c6d8ed67fa881d1fd877a0403af85e6ae8a59cd
NetAcad DEVASC <changeme@example.com>
2026-05-21 03:31:43 +0000

    Aplicacion Flask puerto 9999

--- sample_app.py
+++ sample_app.py
@@ -1,1 +1,10 @@
-# Add to this file for the sample app lab
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Aplicacion Docker Jenkins funcionando en puerto 9999</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9999)
